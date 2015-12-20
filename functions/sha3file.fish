a21d38c11ba5b9f6b9d3f1c8068676a09694dd23
Fabio Tea <ft2011@gmail.com>
2015-12-20 04:18:14 +0100

    default autosave

--- functions/sha3file.fish
+++ functions/sha3file.fish
@@ -1,23 +1,17 @@
 function sha3file -d "Install specific package(s) from the repositories"
 	set argCnt (count $argv)
 	set argFile $argv[1]
+
 	if $argCnt == 0 or $argCnt > 2 or not test -f "$argv[1]"
 		echo "usage: sha3file [path to file] [given sha1sum]"
 		return
 	end
 
-
-
-	if $argCnt == 1
+	if (eval (count $argv)) == 1
 		echo "rhash --sha3-512 $argv[1] > $argv[1].sha3"
-		rhash --sha3-512 "$argv[1]" > "$argv[1]".sha3
+		rhash --sha3-512 "$argv[1]" > "$argv[1].sha3"
 	else if $argCnt == 2 and not test -f "$argv[1]"
-		echo "$argv[1]" (basename "$argv[1]") | tee "$argv[1]".sha3
+		echo "$argv[1]" (basename "$argv[1]") | tee "$argv[1].sha3"
 	end
-
-	if 1==2
-	    echo "bla"
-	end
-
 	rhash -c "$argv[1]".sha3
 end
