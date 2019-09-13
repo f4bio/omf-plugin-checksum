930773ac230e0bb2fd9e42ed321732f8497dd799
Fabio Tea <iam@f4b.io>
2019-09-13 16:29:51 +0200

    save

--- init.fish
+++ init.fish
@@ -1,5 +1,3 @@
-function init --on-event init_rhash
-	if not available rhash
-		echo "Please install rhash first. See https://github.com/rhash/RHash"
-	end
+if not available rhash
+    echo "Please install rhash first. See https://github.com/rhash/RHash"
 end
