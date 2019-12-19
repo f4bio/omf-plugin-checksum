bc9e2b4e50f18a6b8f1c2606fce480e86394d6ff
Fabio Tea <iam@f4b.io>
2019-12-19 16:06:08 +0100

    save

--- init.fish
+++ init.fish
@@ -1,3 +1,5 @@
-if not available rhash
+env RHASH_VERSION=`rhash --version 2>/dev/null`
+
+if test -z "$RHASH_VERSION" ; then
     echo "Please install rhash first. See https://github.com/rhash/RHash"
 end
